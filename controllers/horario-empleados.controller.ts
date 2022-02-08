8b1beeb85ba50ab7e5ac415eedf94e9941f00723
Miguel Angel López Luciano <benzin_97@hotmail.com>
2022-02-08 15:18:16 -0600

    Agregado HorarioEmpleados CRUD Completo

--- controllers/horario-empleados.controller.ts
+++ controllers/horario-empleados.controller.ts
@@ -25,12 +25,12 @@ export const getHorarioEmpleadoById = async (request: Request, response: Respons
     });
   }
 
-  const carrera = await HorarioEmpleados.findByPk(idHorarioEmpleado);
+  const horariosEmpleado = await HorarioEmpleados.findByPk(idHorarioEmpleado);
 
-  if (carrera)
+  if (horariosEmpleado)
   {
     response.json({
-      data: carrera,
+      data: horariosEmpleado,
       success: true,
       message: 'Datos obtenidos correctamente'
     });
@@ -39,9 +39,200 @@ export const getHorarioEmpleadoById = async (request: Request, response: Respons
   {
     response.status(404).json({
       idHorarioEmpleado,
-      data: carrera,
+      data: horariosEmpleado,
       success: false,
       message: 'No existe registro con el id ' + idHorarioEmpleado
     });
   }
+}
+
+export const getHorariosByEmpleadoByIdEmpleado = async (request: Request, response: Response) => {
+  
+  const idEmpleado = Number(request.params.idEmpleado);
+
+  if (isNaN(idEmpleado))
+  { 
+    return response.status(400).json({
+      data: null,
+      success: false,
+      message: 'El idEmpleado no es un valor válido'
+    });
+  }
+
+  // const horariosEmpleado = await HorarioEmpleados.findByPk(idHorarioEmpleado);
+
+  const horariosEmpleado = await HorarioEmpleados.sequelize?.query("SELECT * FROM horariosempleados WHERE fk_idEmpleado = ?", {
+    replacements: [idEmpleado],
+    model: HorarioEmpleados,
+    mapToModel: true
+  });
+
+  if (horariosEmpleado)
+  {
+    response.json({
+      data: horariosEmpleado,
+      success: true,
+      message: 'Datos obtenidos correctamente'
+    });
+  }
+  else
+  {
+    response.status(404).json({
+      idEmpleado,
+      data: horariosEmpleado,
+      success: false,
+      message: 'No existe registro con el id ' + idEmpleado
+    });
+  }
+}
+
+export const createHorarioEmpleado = async (request: Request, response: Response) => {
+
+  const body =  request.body;
+
+  try
+  {
+    const horarioEmpleado = HorarioEmpleados.build(body);
+    await horarioEmpleado.save();
+
+    //Guardamos el resultado de la consulta ejecutada
+    const resultCreate: any = horarioEmpleado;
+
+    //Objeto con los datos creados para mandar como respuesta
+    const dataCreated = {
+      idHorarioEmpleado: resultCreate.null,
+      fk_idEmpleado: resultCreate.fk_idEmpleado,
+      mes: resultCreate.mes,
+      anio: resultCreate.anio,
+      datosHorario: resultCreate.datosHorario,
+      estatus: resultCreate.estatus,
+      createdAt: resultCreate.createdAt,
+      updatedAt: resultCreate.updatedAt
+    };
+
+    response.json({
+      data: dataCreated,
+      success: true,
+      message: 'Datos guardados correctamente'
+    });
+  }
+  catch (error)
+  {
+    response.status(500).json({
+      error: error,
+      success: false,
+      message: 'Error al procesar la petición'
+    });
+  }
+}
+
+export const updateHorarioEmpleado = async (request: Request, response: Response) => {
+
+  const body = request.body;
+
+  try
+  {
+    //Si en el body del response no viene el 'idHorarioEmpleado'
+    if (!body.idHorarioEmpleado)
+    {
+      return response.status(400).json({
+        data: null,
+        success: false,
+        message: 'El idHorarioEmpleado es requerido'
+      });
+    }
+
+    const horarioEmpleado = await HorarioEmpleados.findByPk(body.idHorarioEmpleado);
+
+    //Si no existe registro del idHorarioEmpleado proporcionado
+    if (!horarioEmpleado)
+    {
+      return response.status(404).json({
+        data: horarioEmpleado,
+        success: false,
+        message: 'No existe registro con el id ' + body.idHorarioEmpleado
+      });
+    }
+
+    await horarioEmpleado.update(body);
+
+    response.json({
+      data: horarioEmpleado,
+      success: true,
+      message: 'Datos actualizados correctamente'
+    });
+  }
+  catch (error)
+  {
+    response.status(500).json({
+      error: error,
+      success: false,
+      message: 'Error al procesar la petición'
+    });
+  }
+}
+
+export const updateEstatusHorarioEmpleado = async (request: Request, response: Response) => {
+
+  const idHorarioEmpleado = Number(request.params.idHorarioEmpleado);
+
+  const estatus = request.query.estatus;
+
+  // const body = request.body
+  // const estatus = body.estatus
+
+  if (isNaN(idHorarioEmpleado))
+  { 
+    return response.status(400).json({
+      data: null,
+      success: false,
+      message: 'El idHorarioEmpleado no es un valor válido'
+    });
+  }
+
+  const horarioEmpleado = await HorarioEmpleados.findByPk(idHorarioEmpleado);
+
+  if (!horarioEmpleado)
+  {
+    return response.status(404).json({
+      data: horarioEmpleado,
+      success: false,
+      message: 'No existe registro con el id ' + idHorarioEmpleado
+    });
+  }
+
+  if (estatus == undefined)
+  {
+    return response.status(400).json({
+      data: null,
+      success: false,
+      message: 'El valor del estatus es requerido (true o false)'
+    });
+  }
+
+  //Habilitar o deshabilitar un registro (Update estatus)
+  if (estatus == 'true')
+  {
+    //Si el estatus viene con valor 'true' deshabilita el registro
+    horarioEmpleado.update({ estatus: false });
+  }
+  else if (estatus == 'false')
+  {
+    //Si el estatus viene con valor 'false' habilita el registro
+    horarioEmpleado.update({ estatus: true });
+  }
+  else
+  {
+    return response.status(400).json({
+      data: null,
+      success: false,
+      message: 'El valor del estatus no es válido (true o false)'
+    });
+  }
+
+  response.json({
+    data: horarioEmpleado,
+    success:  true,
+    message: 'Estatus actualizado correctamente',
+  });
 }
--- routes/horario-empleado.routes.ts
+++ routes/horario-empleado.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { getAllHorariosEmpleados, getHorarioEmpleadoById } from '../controllers/horario-empleados.controller';
+import { getAllHorariosEmpleados, getHorarioEmpleadoById, getHorariosByEmpleadoByIdEmpleado, createHorarioEmpleado, updateHorarioEmpleado, updateEstatusHorarioEmpleado } from '../controllers/horario-empleados.controller';
 
 const router = Router();
 
 router.get('/', getAllHorariosEmpleados);
 router.get('/:idHorarioEmpleado', getHorarioEmpleadoById);
-// router.post('/', createCarrera);
-// router.put('/', updateCarrera);
-// router.delete('/:idCarrera', disableCarrera);
+router.get('/Empleado/:idEmpleado', getHorariosByEmpleadoByIdEmpleado);
+router.post('/', createHorarioEmpleado);
+router.put('/', updateHorarioEmpleado);
+router.delete('/:idHorarioEmpleado', updateEstatusHorarioEmpleado);
 
 export default router;
